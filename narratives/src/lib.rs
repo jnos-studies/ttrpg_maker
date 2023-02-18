92a517d1c2aa8c6563120bbdcf43853df1f96676
writatas <jnelson.writing@gmail.com>
2023-02-18 10:45:39 +0100

    Added to the narratives library the structs TypedNarrative, AutoNarrative, and Rolled. TypedNarratives are the raw text, original input, and AutoNarratives are TypedNarratives which are to become summarized. Rolled is for Typed Narratives and their association with a rolled dice number, corresponding to a Hashmap which will be implemented in the future.

--- narratives/src/lib.rs
+++ narratives/src/lib.rs
@@ -1,2 +1,45 @@
 use roll_dice::*;
 
+trait AutoText {
+    fn summarize(&mut self, text: &mut TypedNarrative) -> String;
+}
+
+trait Rolled {
+    fn roll_to_text(roll: &Roll) -> String;
+}
+
+// literally raw, original text
+pub struct TypedNarrative {
+    text: String,
+}
+
+impl TypedNarrative {
+    pub fn new(text: String) -> TypedNarrative {
+        TypedNarrative {
+            text,
+        }
+    }
+}
+
+pub struct AutoNarrative {
+    summary: String,
+}
+
+impl AutoNarrative {
+    pub fn new(&mut self, text: &mut TypedNarrative) -> AutoNarrative {
+        let summary = self.summarize(text);
+        AutoNarrative {
+            summary
+        }
+    }
+}
+impl AutoText for AutoNarrative {
+    fn summarize(&mut self, text: &mut TypedNarrative) -> String {
+        let summary = text.text.clone(); // later actually summarize. Functionality is not implemented yet
+        summary
+    }
+}
+
+
+pub struct TabledNarratives;
+
