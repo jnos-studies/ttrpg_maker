07bf4329002fb670b51c93580d9d6b0bc637c7ef
writatas <jnelson.writing@gmail.com>
2023-03-07 14:29:12 +0100

    Working on the record_me module, havent quiet gotten it down yet.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use libtext;
+use std::path::Path;
 
 fn main(){
+    let record_path = Path::new("test.wav");
+    libtext::record_audio(record_path).unwrap();
     let test = libtext::transcribe_audio_file("test.wav");    
     println!("Audio file transcribed: {}", test.text);
 }
