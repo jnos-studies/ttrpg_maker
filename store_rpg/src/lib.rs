e2287f9dad0f8e4de74218271053fe8395768721
writatas <jnelson.writing@gmail.com>
2023-03-16 19:06:33 +0100

    Implemented a database set up function in store_rpg crate. This will enable multible saves of different ttrpgs. Working on a save function for saving particular entities in their respective database

--- store_rpg/src/lib.rs
+++ store_rpg/src/lib.rs
@@ -1,70 +1,99 @@
 use sqlite;
 //use entities::*;
-use std::path::Path;
-pub fn database_setup(database_path: &str) {
-    let database_path = Path::new(database_path);
-    let connection = sqlite::open(database_path).unwrap();
-    let query = "
-        CREATE TABLE ttrpgs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date DATETIME DEFAULT CURRENT_TIMESTAMP,
-            name TEXT NOT NULL
-        );
-        CREATE TABLE stories (
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpgs(id),
-            label TEXT NOT NULL,
-            text_data TEXT NOT NULL
-        );
-        CREATE TABLE attributes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpgs(id),
-            label TEXT NOT NULL,
-            description TEXT NOT NULL,
-            outcome FOREIGN KEY (outcome_id) REFERENCES attribute_outcomes(attribute_id)
-        );
-        CREATE TABLE attribute_outcomes (
-            FOREIGN KEY (attribute_id) REFERENCES attributes(id),
-            roll_description TEXT NOT NULL,
-            base_result INTEGER NOT NULL,
-            max INTEGER NOT NULL,
-            min INTEGER NOT NULL
-        );
-       CREATE TABLE skills (
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id),
-            label TEXT NOT NULL,
-            description TEXT NOT NULL,
-            roll FOREIGN KEY (roll_id) REFERENCES rolls(skill_id)
-       );
 
-       CREATE TABLE rolls (
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id),
-            skill_id INTEGER,
-            blank_roll INTEGER NOT NULL,
-            dice_label TEXT NOT NULL,
-            dice INTEGER NOT NULL,
-            amount INTEGER NOT NULL
-       );
-       
-       CREATE TABLE counters (
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id),
-            label TEXT NOT NULL,
-            description TEXT NOT NULL,
-            number INTEGER NOT NULL
-       );
+pub fn save_entity<T>(entity:T, database_path: &str) {
+    let connection = sqlite::Connection::open(database_path).unwrap();
 
-       CREATE TABLE tables (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id),
-            label TEXT NOT NULL,
-            description TEXT,
-       );
-       
-       CREATE TABLE table_values (
-            FOREIGN KEY (table_id) REFERENCES tables(id),
-            lower_range INTEGER NOT NULL,
-            higher_range INTEGER NOT NULL,
-            text_value TEXT NOT NULL
-       );
-    ";
-    connection.execute(query).unwrap();
+    connection.execute("INSERT INTO ttrpgs(name) VALUES('test')").unwrap();
+    let mut ttrpgs = Vec::new();
+
+    connection.iterate("SELECT id, name, date FROM ttrpgs", |row| {
+    ttrpgs.push(
+        (
+            row[0].1.unwrap().to_string(),
+            row[1].1.unwrap().to_string(),
+            row[2].1.unwrap().to_string(),
+        )
+    );
+    true
+}).unwrap();
+    
+    println!("{:#?}", ttrpgs);
+}
+
+pub fn database_setup(database_path: &str) { 
+    let connection = sqlite::Connection::open(database_path).unwrap();
+
+    connection.execute("CREATE TABLE ttrpgs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        name TEXT NOT NULL
+    )").expect("Could not create initial table");
+    
+    connection.execute("CREATE TABLE stories (
+        ttrpg_id INTEGER NOT NULL,
+        label_text TEXT NOT NULL,
+        text_data TEXT NOT NULL,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpgs(id)
+    )").expect("Could not create table");
+    
+    connection.execute("CREATE TABLE attributes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ttrpg_id INTEGER NOT NULL,
+        label_text TEXT NOT NULL,
+        description TEXT NOT NULL,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpgs(id)
+    )").expect("Could not create table");
+
+    connection.execute("CREATE TABLE attribute_outcomes (
+        attribute_id INTEGER NOT NULL,
+        roll_description TEXT NOT NULL,
+        base_result INTEGER NOT NULL,
+        max INTEGER NOT NULL,
+        min INTEGER NOT NULL,
+        FOREIGN KEY (attribute_id) REFERENCES attributes(id)
+    )").expect("Could not create table");
+    
+    connection.execute("CREATE TABLE rolls (
+        ttrpg_id INTEGER NOT NULL,
+        skill_id INTEGER,
+        blank_roll INTEGER NOT NULL,
+        dice_label TEXT NOT NULL,
+        dice INTEGER NOT NULL,
+        amount INTEGER NOT NULL,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id)
+    )").expect("Could not create table");
+
+    connection.execute("CREATE TABLE skills (
+        ttrpg_id INTEGER NOT NULL,
+        roll_id INTEGER NOT NULL,
+        label_text TEXT NOT NULL,
+        description TEXT NOT NULL,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id),
+        FOREIGN KEY (roll_id) REFERENCES rolls(skill_id)
+    );").expect("Could not create table");
+  
+    connection.execute("CREATE TABLE counters (
+        ttrpg_id INTEGER NOT NULL,
+        label_text TEXT NOT NULL,
+        description TEXT NOT NULL,
+        number INTEGER NOT NULL,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id)
+    )").expect("Could not create table");
+  
+    connection.execute("CREATE TABLE tables (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ttrpg_id INTEGER NOT NULL,
+        label_text TEXT NOT NULL,
+        description TEXT,
+        FOREIGN KEY (ttrpg_id) REFERENCES ttrpg(id)
+    )").expect("Could not create table");
+  
+    connection.execute("CREATE TABLE table_values (
+        table_id INTEGER NOT NULL,
+        lower_range INTEGER NOT NULL,
+        higher_range INTEGER NOT NULL,
+        text_value TEXT NOT NULL,
+        FOREIGN KEY (table_id) REFERENCES tables(id)
+    )").expect("Could not create table");
 }
