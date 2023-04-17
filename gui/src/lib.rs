aa5841eb5447e3b8f55492c49052faa5763ec82f
writatas <jnelson.writing@gmail.com>
2023-04-17 13:53:21 +0200

    Implemented the beginnings of the ui which will handle the loading and creation of new databases, As well as there ttrpg elements. CRUD is implemented throughout the application

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use eframe::egui::TextBuffer;
 use eframe::epaint::image;
 use regex::Regex;
 use entities::*;
@@ -72,7 +73,6 @@ impl Default for TTRPGMaker
 impl eframe::App for TTRPGMaker {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         // Create a UI with a label and a combo box
-        frame.set_centered();
         egui::TopBottomPanel::top("tabs")
             .show_separator_line(false)
             .show(ctx, |ui| {
@@ -81,23 +81,22 @@ impl eframe::App for TTRPGMaker {
                     ui.available_rect_before_wrap(),
                     egui::Layout::left_to_right(egui::Align::Center)
                 );
-                
                 let stroke = egui::Stroke::new(1.0, egui::Color32::GOLD);
-
                 let elements_button = tabs.add(egui::Button::new("Elements").stroke(stroke));
                 let creation_button = tabs.add(egui::Button::new("Create").stroke(stroke));
                 let load_button = tabs.add(egui::Button::new("Load").stroke(stroke));
                 
-
                 if elements_button.clicked()
                 {
                     self.load_elements.set(true);
                     self.load_creation.set(false);
+                    self.load_database.set(false);
                 }
                 if creation_button.clicked()
                 {
                     self.load_elements.set(false);
                     self.load_creation.set(true);
+                    self.load_database.set(false);
                 }
                 if load_button.clicked()
                 {
@@ -108,26 +107,65 @@ impl eframe::App for TTRPGMaker {
 
         if self.load_database.get()
         { 
-            egui::Window::new("Load Database")
+            egui::Window::new("Database and TTRPG creator")
                 .collapsible(false)
                 .resizable(false)
                 .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::new(0.0, 0.0))
-                .resize(|r| {r.min_size(egui::Vec2::new(20.0, 10.0))})
                 .show(ctx, |ui| {
-                    ui.with_layout(egui::Layout::top_down_justified(egui::Align::Center), |ui|{
-                        ui.add_space(10.0);                                                            
-                        ui.set_width(ctx.available_rect().width());                                    
-                        ui.set_height(ctx.available_rect().height());
+                    ui.set_width(ctx.available_rect().width());
+                    ui.set_height(ctx.available_rect().height() - 40.0);
+                    // create child ui elements in the load page that will handle database
+                    // creation and deletion, as well as the creation and deletion of
+                    // ttrpg elements
+                    let mut database_selection_and_creator = ui.child_ui
+                    (
+                        ui.max_rect(),
+                        egui::Layout::top_down_justified(egui::Align::TOP)
+                    );
+                    let mut ttrpg_selection_and_creator = ui.child_ui
+                    (
+                        ui.max_rect().shrink2(egui::vec2(0.0, ui.available_height() / 11.0)),
+                        egui::Layout::top_down_justified(egui::Align::Center)
+                    );
+                    database_selection_and_creator.set_width(ui.available_width());
+                    database_selection_and_creator.set_height(ui.available_height() / 4.0);
+                    ttrpg_selection_and_creator.set_width(ui.available_width());
+                    ttrpg_selection_and_creator.set_height(ui.available_height());
+                    
+                    database_selection_and_creator.group(|ui|
+                    {
+                        ui.label("Select a database");
                         egui::ComboBox::from_id_source("databases")
                             .selected_text(self.selected.as_deref().unwrap_or("None"))
-                            .width(ctx.available_rect().width() / 2.0)
-                            .show_ui(ui, |ui| {
-                                for db in &self.databases {
+                            .show_ui(ui, |ui|
+                            {
+                                for db in &self.databases
+                                {
                                     let selectable_value = ui.selectable_value(&mut self.selected, Some(db.clone()), db);
+                                    if selectable_value.clicked()
+                                    {
+                                        ctx.request_repaint();
+                                    }
                                 }
-                          });  
+                            });
                     });
-                    
+
+                    ttrpg_selection_and_creator.group(|ui|
+                    {
+                        // load existing ttrpgs in database
+                        if self.selected.is_some()
+                        {
+                            let database_path = format!("saves/{}", self.selected.as_deref().unwrap().as_str());
+                            let load_names = store_rpg::get_existing_ttrpgs_from_database(&database_path);
+                            for name in load_names
+                            {
+                                ui.push_id(&name, |ui|
+                                {
+                                    ui.label(&name);               
+                                });
+                            }
+                        }
+                    });
                 });
         }
 
@@ -146,7 +184,6 @@ impl eframe::App for TTRPGMaker {
         }
         if self.selected.is_some()
         {
-            self.load_database.set(false);
             let selected_ref = self.selected.as_deref().unwrap();
             env::set_var("DATABASE_PATH", format!("saves/{}",selected_ref));
         }
